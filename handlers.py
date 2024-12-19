86707445f9ce0d8b38160e99abf0cbbd82c55869
elizaveta212 <116644834+elizaveta212@users.noreply.github.com>
2024-12-19 18:13:49 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,10 +1,14 @@
 from aiogram import F, Router
+from aiogram import types
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.context import FSMContext 
+
 import keyboard as kb
-from database import get_perfumes
+
+import asyncpg
+from database import get_perfumes, create_database, create_tables
 
 router = Router()
 
@@ -32,7 +36,7 @@ async def process_gender(callback: CallbackQuery, state: FSMContext):
     await callback.message.answer(f'Вы выбрали {selected_gender} парфюмерию.')
     await state.update_data(gender=selected_gender)
     await callback.message.answer('Какую группу ароматов Вы предпочитаете?', reply_markup=kb.fragrance_group)
-    await Form.fragrance_group.set()
+    await state.set_state(Form.fragrance_group)
     
 
 @router.callback_query(F.data.in_(['цветочные', 'цитрусовые', 'фруктовые','восточные', 'древесные', 'фужерные', 'гурманские', 'зелёные', 'акватические']))
@@ -61,7 +65,7 @@ async def process_fragrance_group(callback: CallbackQuery, state: FSMContext):
         notes_keyboard = kb.notes_keyboard_aqua
     if notes_keyboard:
         await callback.message.answer('Выберите ноты:', reply_markup=notes_keyboard)
-        await Form.notes.set()
+        await state.set_state(Form.notes)
 
 @router.callback_query(F.data.in_(['роза', 'мимоза', 'ирис', 'лаванда', 'жасмин', 'тубероза', 'бергамот', 'лимон', 'апельсин', 'мандарин', 'юдзу', 'нероли', 
                                    'уд', 'ладан', 'сандал', 'шафран', 'иланг-иланг', 'пачули', 'кедр', 'ель', 'эвкалипт', 'берёза', 'сосна', 'ветивер', 
@@ -73,7 +77,7 @@ async def process_note_selection(callback: CallbackQuery, state: FSMContext):
     await callback.message.answer(f'Вы выбрали ноту: {selected_note}.')
     await state.update_data(notes=selected_note)
     await callback.message.answer('Какой тип парфюма Вам нужен?', reply_markup=kb.occasion)
-    await Form.occasion.set()
+    await state.set_state(Form.occasion)
 
 @router.callback_query(F.data.in_(['повседневный', 'вечерний', 'спортивный']))
 async def process_season(callback: CallbackQuery, state: FSMContext):
@@ -81,7 +85,7 @@ async def process_season(callback: CallbackQuery, state: FSMContext):
     await callback.message.answer(f'Вы выбрали {selected_occasion} аромат.')
     await state.update_data(occasion = selected_occasion)
     await callback.message.answer('На какое время года нужен аромат?', reply_markup=kb.season)
-    await Form.season.set()
+    await state.set_state(Form.season)
 
 @router.callback_query(F.data.in_(['зима', 'весна', 'лето', 'осень']))
 async def process_season(callback: CallbackQuery, state: FSMContext):
@@ -89,23 +93,72 @@ async def process_season(callback: CallbackQuery, state: FSMContext):
     await callback.message.answer(f'Вы выбрали время года {selected_season}.')
     await state.update_data(occasion = selected_season)
     await callback.message.answer('В каком ценовом диапазоне ищем парфюм?', reply_markup=kb.price_range)
-    await Form.price_range.set()
+    await state.set_state(Form.price_range)
 
 @router.callback_query(F.data.in_(['до 5000', 'от 5000 до 10000', 'от 10 000 до 15000', 'от 15 000 и более']))
 async def process_price_range(callback: CallbackQuery, state: FSMContext):
-    selected_price_range= callback.data
+    selected_price_range = callback.data
     await callback.message.answer(f'Вы выбрали ценовой диапазон {selected_price_range}.')
-    await state.update_data(price_range = selected_price_range)
-    await callback.message.answer('Подбираем парфюм для Вас...')
+    await state.update_data(price_range=selected_price_range)
+    await callback.message.answer('Подбираем парфюм для Вас...')  
     user_data = await state.get_data() 
     gender = user_data.get('gender')
-    fragrance_group = user_data.get('fragrance_group')
+    fragrance_groups = user_data.get('group')
     occasion = user_data.get('occasion')
-    price_range = user_data.get('price_range')
     season = user_data.get('season')
-    notes = user_data.get('notes')
-    perfumes = get_perfumes(gender, fragrance_group, occasion, season, price_range, notes)
-    if perfumes:
-        await callback.message.answer("Подобранные ароматы:\n" + "\n".join(perfumes))
-    else:
-        await callback.message.answer("К сожалению, ароматы с такими характеристиками не найдены.")
+    price_range = user_data.get('price_range')
+    note = user_data.get('note')
+    
+    conn = await asyncpg.connect(database='perfum_database', user='postgres', password='0212er', host='localhost', port='5432')
+    
+    async def get_gender_id(conn, gender):
+      query = "SELECT gender_id FROM genders WHERE gender_name = $1"
+      result = await conn.fetchrow(query, gender)
+      return result['gender_id'] if result else None
+        
+    async def get_fragrance_group_id(conn, group):
+      query = "SELECT group_id FROM fragrance_groups WHERE group_name = $1"
+      result = await conn.fetchrow(query, group)
+      return result['group_id'] if result else None
+        
+    async def get_occasion_id(conn, occasion):
+      query = "SELECT occasion_id FROM occasions WHERE occasion_name = $1"
+      result = await conn.fetchrow(query, occasion)
+      return result['occasion_id'] if result else None
+        
+    async def get_season_id(conn, season):
+      query = "SELECT season_id FROM seasons WHERE season_name = $1"
+      result = await conn.fetchrow(query, season)
+      return result['season_id'] if result else None
+        
+    async def get_price_range_id(conn, price_range):
+      query = "SELECT price_range_id FROM price_ranges WHERE price_range_name = $1"
+      result = await conn.fetchrow(query, price_range)
+      return result['price_range_id'] if result else None
+        
+    async def get_note_id(conn, note):
+      query = "SELECT note_id FROM notes WHERE note_name = $1"
+      result = await conn.fetchrow(query, note)
+      return result['note_id'] if result else None
+        
+    try:
+        gender_id = await get_gender_id(conn, gender) 
+        fragrance_group_id = await get_fragrance_group_id(conn, fragrance_groups) 
+        occasion_id = await get_occasion_id(conn, occasion) 
+        season_id = await get_season_id(conn, season) 
+        price_range_id = await get_price_range_id(conn, price_range)
+        note_id = await get_note_id(conn, note)
+        gender_id = await get_gender_id(conn, gender)
+        
+        perfumes = await get_perfumes(conn, gender_id, fragrance_group_id, occasion_id, season_id, price_range_id, note)
+        
+        if perfumes:
+            await callback.message.answer("Подобранные ароматы:\n" + "\n".join(perfumes))
+        else:
+            await callback.message.answer("К сожалению, ароматы с такими характеристиками не найдены.")
+    except Exception as _ex:
+        await callback.message.answer("[INFO] Ошибка при извлечении ароматов: " + str(_ex))
+    finally:
+        await conn.close()
+        print("[INFO] Завершение соединения с PostgreSQL")
+
